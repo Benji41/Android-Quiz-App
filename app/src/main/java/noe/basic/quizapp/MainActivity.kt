583fd831c54cc77b0b4db667e76a0518297700b4
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-07-24 13:37:40 -0700

    +Primera letra en mayusculas y validar si puras letras contiene un string +cambios en UI de activity main

--- app/src/main/java/noe/basic/quizapp/MainActivity.kt
+++ app/src/main/java/noe/basic/quizapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package noe.basic.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editVName = findViewById<EditText>(R.id.edittext_name)
+        //ALLOW THAT THE FIRST Letter IS CAP
+        editVName.inputType= InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
+        val btnVRegisterName = findViewById<Button>(R.id.button_submit_name)
+        btnVRegisterName.setOnClickListener {
+            //Iterate throw a lambda expression with .all all the characters of a string validating if it is a letter
+            if(editVName.text.isNotEmpty() && editVName.text.all{
+                it.isLetter()
+                }){
+                val intent = Intent(this,QuizActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
